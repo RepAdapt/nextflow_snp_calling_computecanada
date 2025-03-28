67b03e8e3641346f3b4bf135bded437051e95e8e
Gabriele <g.nocchi@hotmail.com>
2025-03-28 12:09:37 +0100

    Add files via upload

---
+++ run_pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=6-23:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=def-yeaman
+
+module load nextflow
+module load apptainer
+
+source /home/gabnoc/nf-core-env/bin/activate
+
+export NXF_SINGULARITY_CACHEDIR=/project/def-yeaman/NXF_SINGULARITY_CACHEDIR
+
+
+nextflow run . --ref_genome=/home/gabnoc/scratch/nextflow_stuff/data/ref/Betula_pendula_subsp._pendula.fasta --gff_file=/home/gabnoc/scratch/nextflow_stuff/data/genes/Betula_pendula_subsp._pendula_annos1-cds0-id_typename-nu1-upa1-add_chr0.gid35080.gff -profile narval -config nextflow.config -w ./work/
