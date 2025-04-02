9f11a0b2c4c8b408582a55ebd4e113eabeca0677
Gabriele <g.nocchi@hotmail.com>
2025-04-02 13:12:52 +0200

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -12,4 +12,4 @@ source /home/gabnoc/nf-core-env/bin/activate
 export NXF_SINGULARITY_CACHEDIR=/project/def-yeaman/NXF_SINGULARITY_CACHEDIR
 
 
-nextflow run . --ref_genome=/home/gabnoc/scratch/nextflow_stuff/data/ref/Betula_pendula_subsp._pendula.fasta --gff_file=/home/gabnoc/scratch/nextflow_stuff/data/genes/Betula_pendula_subsp._pendula_annos1-cds0-id_typename-nu1-upa1-add_chr0.gid35080.gff -profile narval -config nextflow.config -w ./work/
+nextflow run main.nf --ref_genome=/home/gabnoc/scratch/nextflow_stuff/data/ref/Betula_pendula_subsp._pendula.fasta --gff_file=/home/gabnoc/scratch/nextflow_stuff/data/genes/Betula_pendula_subsp._pendula_annos1-cds0-id_typename-nu1-upa1-add_chr0.gid35080.gff -profile narval -config nextflow.config -w ./work/
